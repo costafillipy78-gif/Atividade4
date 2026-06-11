ca29fa55d5e78d4158a63a2d947d7e01265a103d
Fillipy Costa Da Silva <20241TIIMI0151@cefetes.br>
2026-06-11 12:45:43 -0300

    Finalizado

---
+++ app/src/main/java/com/example/fillipy/ui/SplashFragment.kt
@@ -0,0 +1,41 @@
+package com.example.fillipy.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fillipy.R
+import com.example.fillipy.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding : FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({checkAuth()},3000)
+    }
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_autentication)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+}
